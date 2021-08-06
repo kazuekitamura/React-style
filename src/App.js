1abc2c7c4304400856b74b56538c25a5c956cc55
kazue kitamura <73727763+kazuekitamura@users.noreply.github.com>
2021-08-06 15:38:46 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { CssModules } from "./components/CssModules";
+import { Emotion } from "./components/Emotion";
+
+import { InlineStyle } from "./components/InlineStyle";
+import { StyledComponets } from "./components/StyledComponents";
+import { StyledJsx } from "./components/StyledJsx";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponets />
+      <Emotion />
+    </div>
+  );
+}
---
+++ src/components/StyledComponents.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const StyledComponets = () => {
+  return (
+    <SContainer>
+      <STitle>-Styled componets-</STitle>
+      <SButton>FIGHT!</SButton>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  border: solid 2px #392eff;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+const STitle = styled.p`
+  margin: 0;
+  color: #3d84a8;
+`;
+const SButton = styled.button`
+  backgroun-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
